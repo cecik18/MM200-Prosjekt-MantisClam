6e534810c12323d05a8bd68ca8cbea49bc390e30
stiansenhl <70133713+stiansenhl@users.noreply.github.com>
2020-10-30 13:42:01 +0100

    Server oppsett
    Forhåpentligvis

---
+++ Stian/Server/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const pg = require("pg");
+const server = express();
+
+server.use(bodyParser.json());
+server.use(express.static('public'));
+
+/* ************************************** */
+server.set('port', (process.env.PORT || 8080));
+server.listen(server.get('port'), function() {
+    console.log('server running', server.get('port'));
+});
