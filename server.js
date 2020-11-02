b7939885305f053c169b7cb8537110e18a2f1c38
stiansenhl <70133713+stiansenhl@users.noreply.github.com>
2020-11-02 11:16:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const pg = require("pg");
 const server = express();
+
+const { Client } = require('pg');
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 
 server.use(bodyParser.json());
 server.use(express.static('public'));
