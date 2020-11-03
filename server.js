6a60d5fe6e3a0de3f2073740d160a6f3329e32aa
stiansenhl <70133713+stiansenhl@users.noreply.github.com>
2020-11-03 15:26:03 +0100

    test

--- server.js
+++ server.js
@@ -1,31 +1,57 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const pg = require("pg");
+const {
+    encrypt,
+    decrypt
+} = require("./modules/cesarcipher");
+
+const credentials = process.env.DATABASE_URL || require("./NEI").credentials;
+
+const db = new(require("./modules/storagehandler"))(credentials);
+
 const server = express();
-
-const { Client } = require('pg');
-
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
-
-server.use(bodyParser.json());
-server.use(express.static('public'));
-
-/* ************************************** */
 server.set('port', (process.env.PORT || 8080));
-server.listen(server.get('port'), function() {
+server.use(express.static('public'));
+server.use(bodyParser.json());
+
+
+// 1. Lagre meldinger ?
+
+let meldinger = [];
+
+server.post("/secret", async function (req, res) {
+
+    const message = req.body.message;
+    const secretKey = req.body.secretKey;
+    const cipherText = encrypt(message, secretKey);
+
+    let result = await db.saveSecret(cipherText);
+    if (result instanceof Error) {
+        res.status(500).end()
+    } else {
+        res.status(200).json({
+            "secretId": result
+        }).end();
+    }
+
+});
+
+// 2. Hente meldinger ?
+
+server.get("/secret/:id/:key", async function (req, res) {
+
+    const cipherText = await db.retrieveSecret(req.params.id);
+    const message = decrypt(cipherText, req.params.key);
+
+    res.status(200).json({
+        message: message
+    }).end()
+
+});
+
+
+
+server.listen(server.get('port'), function () {
     console.log('server running', server.get('port'));
 });
