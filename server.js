9c7ba55614225de7e181631b3bcbab432a2d1c82
stiansenhl <70133713+stiansenhl@users.noreply.github.com>
2020-11-07 17:01:46 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,73 +1,30 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const {
-    Router
-  } = require('express');
-  const secureEndpoints = require("./modules/secureEndpoints")
-  const user = require("./modules/user")
-
-const pg = require("pg");
-const {
-    encrypt,
-    decrypt
-} = require("./modules/cesarcipher");
-
-const credentials = process.env.DATABASE_URL;
-
-const db = new(require("./modules/storagehandler"))(credentials);
+  Router
+} = require('express');
+const secureEndpoints = require("./modules/secureEndpoints")
+const user = require("./modules/user")
 
 const server = express();
-server.set('port', (process.env.PORT || 8080));
+const port = (process.env.PORT || 8080);
+
+
+server.set('port', port);
 server.use(express.static('public'));
 server.use(bodyParser.json());
+// https://expressjs.com/en/guide/routing.html
 server.use("/secure", secureEndpoints);
 
-// Lage bruker
 
 server.post("/user", async function (req, res) {
-    const newUser = new user(req.body.username, req.body.password);
-    await newUser.create();
-    res.status(200).json(newUser).end();
-  });
-
-
-// 1. Lagre meldinger ?
-
-let meldinger = [];
-
-server.post("/user", async function (req, res) {
-
-    const inputEmailLogin = req.body.inputEmailLogin;
-    const inputPasswordLogin = req.body.inputPasswordLogin;
-    const cipherText = encrypt(inputEmailLogin, inputPasswordLogin);
-
-    let result = await db.saveUser(cipherText);
-    if (result instanceof Error) {
-        res.status(500).end()
-    } else {
-        res.status(200).json({
-            "loginId": result
-        }).end();
-    }
-
-});
-
-// 2. Hente meldinger ?
-
-server.get("/secret/:id/:key", async function (req, res) {
-
-    const cipherText = await db.retrieveSecret(req.params.id);
-    const message = decrypt(cipherText, req.params.key);
-
-    res.status(200).json({
-        message: message
-    }).end()
-
+  const newUser = new user(req.body.username, req.body.password);
+  await newUser.create();
+  res.status(200).json(newUser).end();
 });
 
 
 
 server.listen(server.get('port'), function () {
-    console.log('server running', server.get('port'));
+  console.log('server running', server.get('port'));
 });
